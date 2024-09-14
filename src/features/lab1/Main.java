cabe0438f7b345d9d1d96f9c467d547dead21b55
Max Bushuev <bushuevmaxim55@gmail.com>
2024-09-14 11:36:54 +0600

    Added text loading

--- src/features/lab1/Main.java
+++ src/features/lab1/Main.java
@@ -4,11 +4,18 @@ public class Main {
 
     public static void main(String args[]) {
 
-        IWordCounter counter = new WordCounterImpl();
+        final IWordCounter counter = new WordCounterImpl();
 
-        String exampleText = "На сегодняшний момент язык Java является одним из самых распространенных и популярных языков программирования. Первая версия языка появилась еще в 1996 году в недрах компании Sun Microsystems, впоследствии поглощенной компанией Oracle. Java задумывался как универсальный язык программирования, который можно применять для различного рода задач. И к настоящему времени язык Java проделал большой путь, было издано множество различных версий. Текущей версией является Java 22, которая вышла в марте 2024 года.";
+        final ITextLoader textLoader = new TextFileLoader("src/features/lab1/example_text.txt");
 
-        var map = counter.calculate(exampleText);
-        System.out.println(map);
+        try {
+            final String exampleText = textLoader.load();
+            var map = counter.calculate(exampleText);
+            System.out.println(map);
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
     }
 }

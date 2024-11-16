692d49f2087ea45f7e108af1240cd3ecd857b0c1
Max Bushuev <bushuevmaxim55@gmail.com>
2024-11-16 16:39:30 +0600

    Removed comments from file

--- src/features/robot/SearchRobot.java
+++ src/features/robot/SearchRobot.java
@@ -11,6 +11,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.locks.ReentrantLock;
+import java.util.regex.MatchResult;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
@@ -24,17 +25,16 @@ public class SearchRobot {
         ReverseInheritanceIndex inheritanceIndex = new ReverseInheritanceIndex();
 
         try {
-            List<String> javaFiles = Files.walk(Paths.get(directoryPath))
+            List<Path> javaFiles = Files.walk(Paths.get(directoryPath))
                     .filter(Files::isRegularFile)
                     .filter(file -> file.toString().endsWith(".java"))
-                    .map(Path::toString)
                     .collect(Collectors.toList());
 
             ExecutorService executor = Executors.newFixedThreadPool(Thread.activeCount());
 
             List<Callable<ReverseInheritanceIndex>> tasks = new ArrayList<>();
 
-            for (String filePath : javaFiles) {
+            for (Path filePath : javaFiles) {
                 tasks.add(() -> processFile(filePath));
             }
 
@@ -65,18 +65,29 @@ public class SearchRobot {
         }
     }
 
-    private static ReverseInheritanceIndex processFile(String filePath) {
+    private static ReverseInheritanceIndex processFile(Path filePath) {
         ReverseInheritanceIndex inheritanceIndex = new ReverseInheritanceIndex();
 
         try {
-            List<String> lines = Files.readAllLines(Paths.get(filePath));
+            final String fileContent = new String(Files.readAllBytes(filePath)).replaceAll("//.+", "")
+                    .replaceAll("//.*|(\"(?:\\\\[^\"]|\\\\\"|.)*?\")|(?s)/\\*.*?\\*/", "")
+                    .replaceAll("\t+", " ")
+                    .replaceAll("\\r?\\n", " ")
+                    .replaceAll("(/\\*.*\\*/)", " ")
+                    .replaceAll("\".+\"", "")
+                    .replaceAll(" +", " ");;
             String className = null;
 
-            for (String line : lines) {
+            List<String> matcherResult = Pattern.compile("( class | interface )([a-zA-Z ,0-9]+)(\\{)").matcher(fileContent).results()
+                    .map(MatchResult::group)
+                    .collect(Collectors.toList());
+
+            for (String line : matcherResult) {
                 line = line.trim();
                 Matcher classMatcher = Pattern.compile("(class|interface)\\s+(\\w+)").matcher(line);
                 if (classMatcher.find()) {
                     className = classMatcher.group(2);
+
                 }
                 Matcher extendsMatcher = Pattern.compile("extends\\s+(\\w+)").matcher(line);
                 if (extendsMatcher.find() && className != null) {
